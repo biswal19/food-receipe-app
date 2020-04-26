5b6822a2b07be3fe34917e07025334df4b381dba
chandrika <chandrikabiswal1@gmail.com>
2020-04-26 22:56:55 +0530

    Updates

---
+++ precache-manifest.9fbdcca62aae9a159b4ad1ddc6d1aace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76a0c6ebb23cd7da5b4ada064f9c3b7",
+    "url": "/food-receipe-app/index.html"
+  },
+  {
+    "revision": "2d613b6d52676757184a",
+    "url": "/food-receipe-app/static/css/main.c43a5420.chunk.css"
+  },
+  {
+    "revision": "7a2c39b4cf8af7805bb5",
+    "url": "/food-receipe-app/static/js/2.21b1da82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-receipe-app/static/js/2.21b1da82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d613b6d52676757184a",
+    "url": "/food-receipe-app/static/js/main.6034eea5.chunk.js"
+  },
+  {
+    "revision": "287008d1d874935399d8",
+    "url": "/food-receipe-app/static/js/runtime-main.70e0b36f.js"
+  }
+]);
